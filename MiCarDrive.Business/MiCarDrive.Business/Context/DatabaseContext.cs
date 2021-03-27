95ea07b22d94f9a7dd04cc1b7377505adca337f1
VitaliyFilippov2012 <vitaliy.filippov.2012@mail.ru>
2021-03-27 12:29:57 +0300

    code review

--- MiCarDrive.Business/MiCarDrive.Business/Context/DatabaseContext.cs
+++ MiCarDrive.Business/MiCarDrive.Business/Context/DatabaseContext.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
+﻿using Microsoft.EntityFrameworkCore;
 using DBContext.Models;
 
 #nullable disable
@@ -43,7 +41,6 @@ namespace DBContext.Context
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlServer("Server=DESKTOP-V2B67VR\\VITALISQL;Database=MiCarDrive;Trusted_Connection=True;");
             }
         }
