9ab2d59b72b7663fa25c672d6e1f2e63ca4fe715
Виталий Филиппов <vitaliy.filippov.2012@mail.ru>
2021-06-14 21:50:59 +0300

    returned uploadImagesMethod

--- MiCarDrive.Business/MiWebApi/Controllers/UserController.cs
+++ MiCarDrive.Business/MiWebApi/Controllers/UserController.cs
@@ -3,10 +3,13 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Business.Interfaces;
+using Microsoft.AspNetCore.Http;
 using MiWebApi.Aspects;
 using MiWebApi.Helpers;
 using Shared.Filters;
 using Shared.Models;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 
 namespace MiWebApi.Controllers
 {
@@ -14,6 +17,7 @@ namespace MiWebApi.Controllers
     public class UserController : Controller
     {
         private readonly IUsersService _userService;
+        private readonly string _azureConnectionString;
 
         public UserController(IUsersService userService)
         {
@@ -50,6 +54,38 @@ namespace MiWebApi.Controllers
             if (string.IsNullOrWhiteSpace(userId))
                 return false;
             return await _userService.UpdateUserAsync(user);
+        }
+
+        private async Task<string> UploadImageToAzureBlob(IFormFile file)
+        {
+            try
+            {
+                if (file.Length > 0)
+                {
+                    var container = new BlobContainerClient(_azureConnectionString, "micardriveimages");
+                    var createResponse = await container.CreateIfNotExistsAsync();
+                    if (createResponse != null && createResponse.GetRawResponse().Status == 201)
+                    {
+                        await container.SetAccessPolicyAsync(PublicAccessType.Blob);
+                    }
+
+                    var blob = container.GetBlobClient(file.FileName);
+                    await blob.DeleteIfExistsAsync(DeleteSnapshotsOption.IncludeSnapshots);
+
+                    using (var fileStream = file.OpenReadStream())
+                    {
+                        await blob.UploadAsync(fileStream, new BlobHttpHeaders { ContentType = file.ContentType });
+                    }
+
+                    return blob.Uri.ToString();
+                }
+
+            }
+            catch
+            {
+            }
+
+            return null;
         }
     }
 }
